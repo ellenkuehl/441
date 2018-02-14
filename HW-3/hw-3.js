f8cf3c619862fe6d798da3e2c9fed4d68bcb9f0b
Ellen Kuehl <ellen.kuehl1@gmail.com>
2018-02-13 23:49:57 -0700

    HW3

---
+++ HW-3/hw-3.js
@@ -0,0 +1,29 @@
+/**************************************************/
+// PROBLEM 1 - "typeof Data Values"
+
+
+console.log(typeof 3);
+console.log("stringinstringinstringinstring");
+console.log(true);
+
+/**************************************************/
+// PROBLEM 2 - Using Loops to Create Pyramids
+var t;
+for(var x=1; x<11; x++) {
+t = t + #;
+console.log(t);
+};
+
+/**************************************************/
+// PROBLEM 3 - FizzBuzz
+
+for(var i = 1; i <= 100; i++) {
+if(i % 3 == 0  && i % 5 == 0) {
+ console.log("FizzBuzz");
+} else if(i % 5 == 0) {
+console.log("Buzz");
+} else if(i % 3 == 0) {
+ console.log("Fizz");
+} else {
+ console.log(i);
+}};
---
+++ HW-3/readme.md
@@ -0,0 +1,12 @@
+# Another Great Week!
+## Keep on Keepin' on!
+
+This week, I felt a little overwhelmed with the amount of reading we had to do.
+Some of the concepts I didn't understand right away but they made more sense
+as I practiced them hands-on. I really have enjoyed learning about some of
+JavaScript's capabilities! Compared to HTML5 and CSS3, this system is totally different
+and unique. I enjoy how the system can check a True/False statement, add numbers,
+and loop commands.
+I struggled a little bit when writing my commands initially but I am very proud of
+what I came up with. Hopefully it's correct!!!
+ 
